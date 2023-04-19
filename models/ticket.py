2b2f048e67132975e926dd4a69785a7bdd177afc
Hanna <hannadurham3@gmail.com>
2023-04-19 19:45:40 +0100

    Updated commit to make look nicer init

--- models/ticket.py
+++ models/ticket.py
@@ -1,7 +1,9 @@
 class Ticket:
-    def __init__(self, patron_name, show_name, date, price):
+    def __init__(self, patron_name, show_name, date, price, time, place):
 
             self.patron_name = patron_name
             self.show_name = show_name
             self.date = date
             self.price = price
+            self.time =  time
+            self.place = place
--- models/tickets.py
+++ models/tickets.py
@@ -1,8 +1,8 @@
 from models.ticket import Ticket
 
-ticket1 = Ticket("Hanna", "Parade", "19/04/2023", 18.00)
-ticket2 = Ticket("Jonny", "Parade", "19/04/2023", 18.00)
-ticket3 = Ticket("Diane", "Parade", "19/04/2023", 18.00)
-ticket4 = Ticket("Robbie", "Parade", "19/04/2023", 18.00)
+ticket1 = Ticket("Hanna", "Parade", "19/04/2023", 18.00, "19:30", "Playhouse")
+ticket2 = Ticket("Jonny", "Parade", "19/04/2023", 18.00, "19:30", "Playhouse")
+ticket3 = Ticket("Diane", "Parade", "19/04/2023", 18.00, "19:30", "Playhouse")
+ticket4 = Ticket("Robbie", "Parade", "19/04/2023", 18.00, "19:30", "Playhouse")
 
 tickets = [ticket1, ticket2, ticket3, ticket4]
